36f6e8c03859a32bd6a8f42c2a87d23ca60c5b0c
rambedade <rambedade2308@gmail.com>
2025-02-13 21:31:39 +0530

    added Header and created another components

---
+++ real-estate-main/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Header from "./components/Header"
+import Footer from './components/Footer'
+
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <Footer />
+    </>
+  )
+}
+
+export default App
