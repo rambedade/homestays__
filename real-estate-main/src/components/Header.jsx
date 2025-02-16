1069ebdac29bc721e8142740ef6f311d012072d1
rambedade <rambedade2308@gmail.com>
2025-02-17 02:04:16 +0530

    Added Property Details Page

--- real-estate-main/src/components/Header.jsx
+++ real-estate-main/src/components/Header.jsx
@@ -4,29 +4,26 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
-      <header className="py-6 mb-12 border-b">
-        <div className="container mx-auto flex justify-between items-center">
-          {/* logo */}
-          <Link to="/">
-            <img src={Logo} alt="" />
+    <header className="py-6 mb-12 border-b bg-gradient-to-b from-gray-200 to-white">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        {/* Logo */}
+        <Link to="/">
+          <img src={Logo} alt="HomeLand" className="h-6 w-auto" />
+        </Link>
+        {/* Buttons */}
+        <div className="flex items-center space-x-4">
+          <Link className="hover:text-violet-900 transition text-gray-700" to="">
+            Log in
           </Link>
-          {/* buttons */}
-          <div className="flex items-center gap-6">
-            <Link className="hover" to="">
-              Log In
-            </Link>
-            <Link
-              className="bg-violet-700  hover:bg-violet-800
-           text-white px-4 py-3 rounded-lg transition"
-              to=""
-            >
-              Sign Up
-            </Link>
-          </div>
+          <Link
+            className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-2 rounded-lg transition"
+            to=""
+          >
+            Sign up
+          </Link>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
 
